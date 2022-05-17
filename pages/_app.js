e219a5281ff25c2747833d9532c376270ca4cd34
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2022-05-17 14:04:52 -0400

    inline styles, width listens for size changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useState, useEffect } from 'react';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [width, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener('resize', updateDimensions);
+    return () => window.removeEventListener('resize', updateDimensions);
+  }, []);
+
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+  };
+
+  const responsive = {
+    mobileWidth: width < 1024,
+  };
+
+  return (
+    <>
+      <Header mobileWidth={responsive.mobileWidth} />
+      <Component {...pageProps} mobileWidth={responsive.mobileWidth} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
