6ee54a695275e5597962372736673172c850552b
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2022-05-17 14:21:05 -0400

    site Joe Keohan for inline styles - https://itnext.io/3-ways-to-implement-responsive-design-in-your-react-app-bcb6ee7eb424 - THANK YOU, you saved my mind!

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import '../styles/globals.css';
 import { useState, useEffect } from 'react';
 import Header from '../components/Header';
 
+// site Joe Keohan for inline styles - https://itnext.io/3-ways-to-implement-responsive-design-in-your-react-app-bcb6ee7eb424 - THANK YOU, you saved my mind!
 function MyApp({ Component, pageProps }) {
   const [width, setWindowWidth] = useState(0);
-
   useEffect(() => {
     updateDimensions();
     window.addEventListener('resize', updateDimensions);
